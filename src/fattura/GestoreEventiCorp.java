2eeb45a92c6f55094c4de70783bb9070963b0b05
Michele <debian@debian.168.1.73>
2026-06-09 15:28:07 +0200

    aggiorna

--- src/fattura/GestoreEventiCorp.java
+++ src/fattura/GestoreEventiCorp.java
@@ -6,6 +6,7 @@ package fattura;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 
 public class GestoreEventiCorp implements ActionListener {
 
@@ -19,21 +20,25 @@ public class GestoreEventiCorp implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == gui.aggiungi) {
-            fatturaOggetto.setArticolo(gui.articolo.getText());
-            fatturaOggetto.setDescrizione(gui.descrizione.getText());
-            fatturaOggetto.setQuantita(gui.qta.getText());
-            fatturaOggetto.setPrezzoPezzo(gui.prezzoPezzo.getText());
-            fatturaOggetto.setImporto(gui.importo.getText());
-            gui.dispose();
-            InterfacciaCorpo f = new InterfacciaCorpo(fatturaOggetto);
-            f.setVisible(true);
+        try {
+            if (e.getSource() == gui.aggiungi) {
+                fatturaOggetto.setArticolo(gui.articolo.getText());
+                fatturaOggetto.setDescrizione(gui.descrizione.getText());
+                fatturaOggetto.setQuantita(gui.qta.getText());
+                fatturaOggetto.setPrezzoPezzo(gui.prezzoPezzo.getText());
+                fatturaOggetto.setImporto(gui.importo.getText());
+                gui.dispose();
+                InterfacciaCorpo f = new InterfacciaCorpo(fatturaOggetto);
+                f.setVisible(true);
 
-        } else {
-            gui.dispose();
-            InterfacciaPiede f = new InterfacciaPiede(fatturaOggetto);
-            f.setVisible(true);
+            } else {
+                gui.dispose();
+                InterfacciaPiede f = new InterfacciaPiede(fatturaOggetto);
+                f.setVisible(true);
 
+            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Errore durante il passaggio dei dati: " + ex.getMessage());
         }
     }
 
